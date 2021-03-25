6ea56d65bcfc61adb49617cdd0e9720b5563342f
chupengc <deer10228@gmail.com>
2021-03-25 17:52:04 -0400

    add tenner csp model 1 code

--- tenner_csp.py
+++ tenner_csp.py
@@ -65,9 +65,69 @@ def tenner_csp_model_1(initial_tenner_board):
        model_1 also constains n-nary constraints of sum constraints for each
        column.
     """
+    n_grid, last_row = initial_tenner_board[0], initial_tenner_board[1]
+    n = len(n_grid)
+    domain = [num for num in range(10)]
 
-    # IMPLEMENT
-    return None, None  # CHANGE THIS
+    csp = CSP("tenner_csp")
+
+    vars = []
+    var_array = []
+    for i in range(n):
+        row_i = n_grid[i]
+        vars_i = []
+        for j in range(10):
+            cell = row_i[j]
+            name = str(i) + str(j)
+            if cell != -1:
+                var = Variable(name, [cell])
+            else:
+                var = Variable(name, domain)
+            vars_i.append(var)
+
+        var_array.append(vars_i)
+        vars.extend(vars_i)
+
+    # binary non-equal constraints
+    for row in var_array:
+        for i in range(10):
+            var1 = row[i]
+            for var2 in row[i + 1:]:
+                name = str(var_array.index(row)) + " - " + str(i) + \
+                       str(row.index(var2))
+                scope = [var1, var2]
+                cons = Constraint(name, scope)
+                tuples = []
+                for val1 in var1.cur_domain():
+                    for val2 in var2.cur_domain():
+                        if val1 != val2:
+                            tuples.append((val1, val2))
+                cons.add_satisfying_tuples(tuples)
+                csp.add_constraint(cons)
+
+    # n-nary sum constraints
+    for i in range(10):
+        target = last_row[i]
+        name = str(i) + ": " + str(target)
+        scope = []
+        domains = []
+        for j in range(n):
+            var = var_array[j][i]
+            scope.append(var)
+            domains.append(var.cur_domain())
+        cons = Constraint(name, scope)
+        tuples = []
+        pad_num = (8 - len(domains)) * [[0]]
+        domains.extend(pad_num)  # pad curr_domain to 8 (maximum n = 8)
+        for tuple in list(itertools.product(domains[0], domains[1], domains[2],
+                                            domains[3], domains[4], domains[5],
+                                            domains[6], domains[7])):
+            if sum(tuple) == target:
+                tuples.append(tuple[:n])
+        cons.add_satisfying_tuples(tuples)
+        csp.add_constraint(cons)
+
+    return csp, var_array
 
 
 ##############################
